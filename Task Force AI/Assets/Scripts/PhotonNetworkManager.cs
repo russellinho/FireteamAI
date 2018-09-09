ed3810cf6ceec8dd103507e0be29868454c4676d
Russell Creswell <rncreswe@ncsu.edu>
2018-09-08 20:09:33 -0400

    Must switch to photon after deprecation of uNet on August 1, 2018

---
+++ Task Force AI/Assets/Scripts/PhotonNetworkManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonNetworkManager : MonoBehaviour {
+
+	void Start() {
+		PhotonNetwork.ConnectUsingSettings ();
+		Debug.Log ("Connecting to photon...");
+	}
+
+	private void OnConnectedToMaster() {
+		PhotonNetwork.JoinLobby (TypedLobby.Default);
+	}
+
+	private void OnJoinedLobby() {
+		Debug.Log ("Lobby joined");
+	}
+
+	private void OnDisconnectedFromPhoton() {
+		Debug.Log ("Disconnected from photon");
+	}
+}
