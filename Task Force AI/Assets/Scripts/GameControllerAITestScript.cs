155ecc3f1d1130ec04e122a8f7769eccd7f4cbce
Russell Creswell <rncreswe@ncsu.edu>
2018-10-31 00:30:59 -0400

    Updated AI

---
+++ Task Force AI/Assets/Scripts/GameControllerAITestScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerAITestScript : MonoBehaviour {
+
+	public GameObject[] redTeam;
+	public GameObject[] blueTeam;
+
+	void Update() {
+		if (redTeam.Length == 0) {
+			redTeam = GameObject.FindGameObjectsWithTag ("Red")
+		}
+
+		if (blueTeam.Length == 0) {
+			blueTeam = GameObject.FindGameObjectsWithTag ("Blue");
+		}
+	}
+
+}
