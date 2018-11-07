d08fd42a4e29e9c4f5ae3710d45569f79bc15ba4
Russell Creswell <rncreswe@ncsu.edu>
2018-11-07 09:31:33 -0500

    Adding lobby system and player customization

---
+++ Task Force AI/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class PlayerData : MonoBehaviour {
+
+	public static PlayerData playerdata;
+
+	public string playername;
+
+	void Awake () {
+		if(playerdata == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			LoadPlayerData ();
+			playerdata = this;
+		}else if(playerdata != this){
+			Destroy(gameObject);
+		}
+
+	}
+
+	public void SavePlayerData()
+	{
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
+
+		PlayerInfo info = new PlayerInfo();
+		info.playername = playername;
+		bf.Serialize(file, info);
+		file.Close();
+	}
+	public void LoadPlayerData()
+	{
+		if(File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
+		{
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+			PlayerInfo info = (PlayerInfo) bf.Deserialize(file);
+			file.Close();
+			playername = info.playername;
+		}
+	}
+
+}
+
+[Serializable]
+class PlayerInfo
+{
+	public string playername;
+}
--- Task Force AI/Assets/Scripts/TitleControllerScript.cs
+++ Task Force AI/Assets/Scripts/TitleControllerScript.cs
@@ -2,26 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleControllerScript : MonoBehaviour {
 
 	public GameObject mainMenu;
 	public GameObject networkMan;
 	public GameObject matchmakingMenu;
+	public GameObject settingsMenu;
+
+	public InputField PlayerNameInput;
 
 	// Use this for initialization
 	void Start () {
+		PlayerData.playerdata.LoadPlayerData();
+		PlayerNameInput.text = PlayerData.playerdata.playername;
 		mainMenu.SetActive (true);
-		matchmakingMenu.SetActive (false);
 	}
 
 	public void GoToMatchmakingMenu() {
 		mainMenu.SetActive (false);
+		settingsMenu.SetActive (false);
 		matchmakingMenu.SetActive (true);
 	}
 
 	public void ReturnToMainMenu() {
+		// Save settings if the settings are active
+		if (settingsMenu.activeInHierarchy) {
+			PlayerData.playerdata.SavePlayerData ();
+		}
+		settingsMenu.SetActive (false);
 		mainMenu.SetActive (true);
 		matchmakingMenu.SetActive (false);
+	}
+
+	public void GoToCustomization() {
+		mainMenu.SetActive (false);
+		matchmakingMenu.SetActive (false);
+		settingsMenu.SetActive (true);
+	}
+
+	public void goToMainMenu (){
+		PlayerPrefs.SetString ("newScene", "MainMenu");
+		SceneManager.LoadScene(7);
+	}
+
+	public void goToTutorial()
+	{
+		PlayerPrefs.SetString ("newScene", "TutorialLevel");
+		SceneManager.LoadScene(7);
+	}
+
+	public void goToTargetPractice() {
+		PlayerPrefs.SetString ("newScene", "TargetPractice");
+		SceneManager.LoadScene (7);
+	}
+
+	public void savePlayerName()
+	{
+		PlayerData.playerdata.LoadPlayerData();
+		PlayerData.playerdata.playername = PlayerNameInput.text;
+		PlayerData.playerdata.SavePlayerData();
 	}
 }
