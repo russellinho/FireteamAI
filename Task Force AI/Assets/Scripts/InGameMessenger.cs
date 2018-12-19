187c2a0b511671ec2f78e2aa3672e312ae72938a
Russell Creswell <rncreswe@ncsu.edu>
2018-12-19 13:25:24 -0500

    Adding messenger

---
+++ Task Force AI/Assets/Scripts/InGameMessenger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMessenger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
