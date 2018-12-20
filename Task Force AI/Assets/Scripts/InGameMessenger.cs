0be0571c51831d22d9cfcd193588f059049a160b
Russell Creswell <rncreswe@ncsu.edu>
2018-12-20 09:19:56 -0500

    Adding messages scripts

--- Task Force AI/Assets/Scripts/InGameMessenger.cs
+++ Task Force AI/Assets/Scripts/InGameMessenger.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class InGameMessenger : MonoBehaviour {
 
+	public GameObject messageInfo;
+	public Queue<GameObject> messageInfos;
+	public TextMeshProUGUI chatText;
+	public TMP_InputField inputText;
+
 	// Use this for initialization
 	void Start () {
-		
+		messageInfos = new Queue<GameObject> ();
+		chatText.text = "";
+		inputText.text = "";
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (messageInfos.Count > 0) {
+			GameObject o = messageInfos.Peek ();
+			// If message is out of time, dequeue it
+			if (o.GetComponent<MessageInfo> ().timeRemaining <= 0f) {
+				chatText.text = chatText.text.Substring (o.GetComponent<MessageInfo>().GetLength());
+				messageInfos.Dequeue ();
+			}
+		}
+	}
+
+	public void Add	qMessage(string message, ) {
 		
 	}
 }
---
+++ Task Force AI/Assets/Scripts/MessageInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageInfo : MonoBehaviour {
+
+	public float timeRemaining;
+	private short lengthOfString;
+
+	// Use this for initialization
+	void Start () {
+		timeRemaining = 0f;
+		lengthOfString = 0;
+	}
+
+	void Update() {
+		timeRemaining -= Time.deltaTime;
+	}
+	
+	public void SetLength(short s) {
+		timeRemaining = 7f;
+		lengthOfString = s;
+	}
+
+	public short GetLength() {
+		return lengthOfString;
+	}
+}
