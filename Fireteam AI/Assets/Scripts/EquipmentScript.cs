76720c5fa6906461043d280c7b87d78274c48721
Russell Creswell <rncreswe@ncsu.edu>
2019-01-21 23:15:03 -0500

    Added aim animation for new player model

---
+++ Fireteam AI/Assets/Scripts/EquipmentScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentScript : MonoBehaviour
+{
+
+	public GameObject primaryWeapon;
+
+	public GameObject weaponSocket;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
