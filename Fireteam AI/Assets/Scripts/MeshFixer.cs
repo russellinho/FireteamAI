b7bb4d15b3467e16d26a3e684582c03a7d319f91
Russell Creswell <rncreswe@ncsu.edu>
2019-01-30 03:31:26 -0500

    Added Lucas clothing and equipment

--- Fireteam AI/Assets/Scripts/MeshFixer.cs
+++ Fireteam AI/Assets/Scripts/MeshFixer.cs
@@ -13,19 +13,17 @@ public class MeshFixer : MonoBehaviour
         Dictionary<string,Transform> boneMap = new Dictionary<string,Transform>();
 
         foreach(Transform bone in targetRenderer.bones) {
-           // Debug.Log(bone.gameObject.name);
+            Debug.Log(bone.gameObject.name);
             boneMap[bone.gameObject.name] = bone;
         }
 
         SkinnedMeshRenderer myRenderer = gameObject.GetComponent<SkinnedMeshRenderer>();
 
-        // foreach(Transform bone in myRenderer.bones) {
-        //     Debug.Log(bone.gameObject.name);
-        // }
         Transform[] newBones = new Transform[myRenderer.bones.Length];
-
-        for(int i = 0; i < myRenderer.bones.Length; ++i) {
+        
+        for(int i = 0; i < myRenderer.bones.Length; i++) {
             GameObject bone = myRenderer.bones[i].gameObject;
+            Debug.Log(bone.name);
             if(!boneMap.TryGetValue(bone.name, out newBones[i])) {
                 Debug.Log("Unable to map bone \"" + bone.name + "\" to target skeleton.");
                 newBones[i] = bone.transform;
