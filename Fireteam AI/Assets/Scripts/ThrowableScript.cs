14f591b70164b23a2f67f04a176fe8faf27d7e1f
Russell Creswell <rncreswe@ncsu.edu>
2019-05-18 23:28:10 -0400

    Added equipping grenade wielding in game

---
+++ Fireteam AI/Assets/Scripts/ThrowableScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowableScript : MonoBehaviour
+{
+
+    public Rigidbody rBody;
+    public SphereCollider col;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        col.enabled = false;
+        rBody.useGravity = false;
+        rBody.isKinematic = true;
+    }
+
+    public void Launch() {
+        // TODO: Fill out
+    }
+}
