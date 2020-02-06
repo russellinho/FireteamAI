48dca154992d6c6934bc36e6afb7cca8e17890a4
Russell Creswell <rncreswe@ncsu.edu>
2020-02-06 01:50:58 -0500

    Created login screen

---
+++ Fireteam AI/Assets/Scripts/AuthScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AuthScript : MonoBehaviour
+{
+    public Text copyrightTxt;
+    public RawImage titleLogoImg;
+    public GameObject popupAlert;
+    public Text popupAlertTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        copyrightTxt.text = DateTime.Now.Year + " ©";
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+
+    // }
+
+    public void ClosePopup() {
+        popupAlert.SetActive(false);
+    }
+
+    void TriggerPopup(string message) {
+        popupAlertTxt.text = message;
+        popupAlert.SetActive(true);
+    }
+}
