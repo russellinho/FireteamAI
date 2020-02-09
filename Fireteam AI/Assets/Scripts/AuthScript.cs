273d908b48bb5c6de596bb44fdaf39dff24b2144
Russell Creswell <rncreswe@ncsu.edu>
2020-02-08 20:10:51 -0500

    Database save/retrieve working

--- Fireteam AI/Assets/Scripts/AuthScript.cs
+++ Fireteam AI/Assets/Scripts/AuthScript.cs
@@ -64,18 +64,40 @@ public class AuthScript : MonoBehaviour
                 return;
             }
 
-            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Firebase.Auth.FirebaseUser user = task.Result;
             //QueuePopup("User signed in successfully: {" + newUser.DisplayName + "} ({" + newUser.UserId + "})");
             // Query DB to see if the user is set up yet. If not, go to setup. Else, go to title page.
+            DAOScript.dao.dbRef.Child("fteam_ai_users").GetValueAsync().ContinueWith(taskA => {
+                if (taskA.IsFaulted) {
+                    QueuePopup("Database is currently unavailable. Please try again later.\nError: " + taskA.Exception);
+                    loginBtn.interactable = true;
+                    return;
+                } else if (taskA.IsCompleted) {
+                    if (!taskA.Result.HasChild(user.UserId)) {
+                        // DAOScript.dao.dbRef.Child("fteam_ai_users").Child(user.UserId).Child("test").SetValueAsync("testyman").ContinueWith(taskB => {
+                        //     if (taskB.IsFaulted) {
+                        //         QueuePopup("Database is currently unavailable. Please try again later.\nError: " + taskB.Exception);
+                        //         loginBtn.interactable = false;
+                        //         return;
+                        //     } else if (taskB.IsCompleted) {
+                        //         ProceedToSetup();
+                        //     }
+                        // });
+                        ProceedToSetup();
+                    } else {
+                        ProceedToTitle();
+                    }
+                }
+            });
         });
     }
 
     public void ProceedToTitle() {
-
+        QueuePopup("Success going to title!");
     }
 
     public void ProceedToSetup() {
-
+        QueuePopup("Success going to setup!");
     }
 
     public void OnRegisterClick() {
--- Fireteam AI/Assets/Scripts/DAOScript.cs
+++ Fireteam AI/Assets/Scripts/DAOScript.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Firebase;
+using Firebase.Database;
 using Firebase.Unity.Editor;
 
 public class DAOScript : MonoBehaviour
 {
     public static DAOScript dao;
+    public DatabaseReference dbRef;
+
     void Awake() {
         if (dao == null)
         {
@@ -21,12 +24,8 @@ public class DAOScript : MonoBehaviour
 
     void Start()
     {
-        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://YOUR-FIREBASE-APP.firebaseio.com/");
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://koobando-web.firebaseio.com/");
+        dbRef = FirebaseDatabase.DefaultInstance.RootReference;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
