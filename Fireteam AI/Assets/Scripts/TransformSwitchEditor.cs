21bc7decdd617c2a5e818f2f2ddc9cae0ddedcc2
Russell Creswell <rncreswe@ncsu.edu>
2020-04-10 14:33:41 -0400

    Finished importing new characters

---
+++ Fireteam AI/Assets/Scripts/TransformSwitchEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(TransformSwitchScript))]
+public class TransformSwitchEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        TransformSwitchScript myScript = (TransformSwitchScript)target;
+        if(GUILayout.Button("Switch Transform"))
+        {
+            myScript.SwitchTransform();
+        }
+    }
+}
---
+++ Fireteam AI/Assets/Scripts/TransformSwitchScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformSwitchScript : MonoBehaviour
+{
+    public Transform obj;
+    public Transform myTrans;
+
+    public void SwitchTransform()
+    {
+        Transform[] childTrans = obj.gameObject.GetComponentsInChildren<Transform>();
+        Transform[] theseTrans = myTrans.gameObject.GetComponentsInChildren<Transform>();
+        for (int i = 0; i < childTrans.Length; i++) {
+            theseTrans[i].position = childTrans[i].position;
+            theseTrans[i].rotation = childTrans[i].rotation;
+            theseTrans[i].localScale = childTrans[i].localScale;
+        }
+    }
+}
