45df9e7eb1e901bbbbca0e68e761d499c6b3088d
Russell Creswell <rncreswe@ncsu.edu>
2020-04-14 03:52:08 -0400

    Finished weapon animations

---
+++ Fireteam AI/Assets/Scripts/WeaponReloadCreatorEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(WeaponReloadCreatorScript))]
+public class WeaponReloadCreatorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        WeaponReloadCreatorScript myScript = (WeaponReloadCreatorScript)target;
+        if(GUILayout.Button("Record Frame"))
+        {
+            myScript.RecordPosition();
+        }
+    }
+}
---
+++ Fireteam AI/Assets/Scripts/WeaponReloadCreatorScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponReloadCreatorScript : MonoBehaviour
+{
+    public int frameIndex;
+    public Vector3[] framePositions;
+    public Transform weaponTransform;
+    public Transform attachToTransform;
+    public Transform partTransform;
+
+    public void RecordPosition() {
+        // Set back to weapon parent
+        partTransform.SetParent(weaponTransform);
+
+        // Record the local transform
+        framePositions[frameIndex] = partTransform.localPosition;
+
+        // Set back to body part transform
+        partTransform.SetParent(attachToTransform);
+    }
+
+}
