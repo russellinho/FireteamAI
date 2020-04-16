f864ca61bc6b00c06ac4d56386258af1e35fb56c
Russell Creswell <rncreswe@ncsu.edu>
2020-04-16 02:42:15 -0400

    Finished weapon stat population

---
+++ Fireteam AI/Assets/Scripts/WeaponCreatorEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(WeaponCreatorScript))]
+public class WeaponCreatorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        WeaponCreatorScript myScript = (WeaponCreatorScript)target;
+        if(GUILayout.Button("Assign Weapon Stats"))
+        {
+            myScript.AssignWeaponStats();
+        }
+    }
+}
---
+++ Fireteam AI/Assets/Scripts/WeaponCreatorScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponCreatorScript : MonoBehaviour
+{
+    public AudioSource fireSource;
+    public AudioSource suppressedFireSource;
+    public AudioSource weaponSoundSource;
+    public WeaponStats weaponStatsRef;
+
+    public void AssignWeaponStats() {
+        weaponStatsRef.fireSound = fireSource;
+        weaponStatsRef.suppressedFireSound = suppressedFireSource;
+        weaponStatsRef.weaponSoundSource = weaponSoundSource;
+    }
+}
