0817b16d152d345a704f12bdde0e73d8ffaf3b62
Russell Creswell <rncreswe@gmail.com>
2020-05-03 15:33:09 -0400

    Added mass add/delete to DB feature

---
+++ Fireteam AI/Assets/Scripts/DatabaseUpdaterEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(DatabaseUpdater))]
+public class DatabaseUpdaterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        DatabaseUpdater myScript = (DatabaseUpdater)target;
+        if(GUILayout.Button("Add Item To All Accounts"))
+        {
+            myScript.AddItemToAllAccounts();
+        }
+        if (GUILayout.Button("Add Melee Category To All Accounts")) {
+            myScript.AddEquippedMeleeFieldToAllAccounts();
+        }
+    }
+}
