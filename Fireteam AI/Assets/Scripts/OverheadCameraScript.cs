824cacf10cd66ecd69bca9b584ec0aceba00d71b
Russell Creswell <rncreswe@gmail.com>
2020-06-13 01:14:15 -0400

    Trying to determine how to cam follow

---
+++ Fireteam AI/Assets/Scripts/OverheadCameraScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverheadCameraScript : MonoBehaviour
+{
+    public Transform mainCamTrans;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.up = mainCamTrans.forward;
+    }
+}
