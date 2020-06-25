2c2eae22180c8affe773b3ab8b54f3af31597495
russellinho <rncreswe@ncsu.edu>
2020-06-25 05:37:56 -0400

    Final minimap fix

--- Fireteam AI/Assets/Scripts/OverheadCameraScript.cs
+++ Fireteam AI/Assets/Scripts/OverheadCameraScript.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class OverheadCameraScript : MonoBehaviour
 {
+    public PhotonView pView;
+    public Transform spineTransform;
     public Transform mainCamTrans;
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.rotation = Quaternion.Euler(90f, 0f, -mainCamTrans.rotation.eulerAngles.y);
+        if (pView.IsMine) {
+            transform.rotation = Quaternion.Euler(90f, 0f, -mainCamTrans.rotation.eulerAngles.y);
+        } else {
+            transform.rotation = Quaternion.Euler(90f, 0f, spineTransform.rotation.eulerAngles.y);
+        }
     }
 }
