9e319bd1e652d59fd720eb178f1f91ebc04539ef
Russell Creswell <rncreswe@gmail.com>
2020-12-03 23:11:51 -0500

    Bullet holes added

--- Fireteam AI/Assets/Scripts/BulletHoleScript.cs
+++ Fireteam AI/Assets/Scripts/BulletHoleScript.cs
@@ -5,21 +5,13 @@ using UnityEngine;
 public class BulletHoleScript : MonoBehaviour {
 
 	public GameObject hitParticles;
-	public AudioClip ricochet1;
-	public AudioClip ricochet2;
-	public AudioClip ricochet3;
+	public AudioClip[] ricochetSounds;
 	public AudioSource a;
 
 	// Use this for initialization
 	void Start () {
-		int r = Random.Range (0, 3);
-		if (r == 0) {
-			a.clip = ricochet1;
-		} else if (r == 1) {
-			a.clip = ricochet2;
-		} else {
-			a.clip = ricochet3;
-		}
+		int r = Random.Range (0, ricochetSounds.Length);
+		a.clip = ricochetSounds[r];
 		a.Play ();
 	}
 
