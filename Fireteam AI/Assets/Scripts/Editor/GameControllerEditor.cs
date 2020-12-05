005586f59c197f235f52ccecc5be8727b0140460
Russell Creswell <rncreswe@gmail.com>
2020-12-05 17:48:17 -0500

    Forgot to remove an editor button

--- Fireteam AI/Assets/Scripts/Editor/GameControllerEditor.cs
+++ Fireteam AI/Assets/Scripts/Editor/GameControllerEditor.cs
@@ -9,16 +9,16 @@ public class GameControllerEditor : Editor
     {
         DrawDefaultInspector();
 
-        if(GUILayout.Button("Assign All"))
-        {
-            GameControllerScript myScript = (GameControllerScript) target;
-            Terrain[] lights = (Terrain[]) GameObject.FindObjectsOfType (typeof(Terrain));
-            myScript.terrainMetaData = new Terrain[lights.Length];
-            for (int i = 0; i < lights.Length; i++) {
-                lights[i].index = i;
-                myScript.terrainMetaData[i] = lights[i];
-            }
-        }
+        // if(GUILayout.Button("Assign All"))
+        // {
+        //     GameControllerScript myScript = (GameControllerScript) target;
+        //     Terrain[] lights = (Terrain[]) GameObject.FindObjectsOfType (typeof(Terrain));
+        //     myScript.terrainMetaData = new Terrain[lights.Length];
+        //     for (int i = 0; i < lights.Length; i++) {
+        //         lights[i].index = i;
+        //         myScript.terrainMetaData[i] = lights[i];
+        //     }
+        // }
 
     }
 }
