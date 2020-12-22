75dfc487193af121bf96fb27ab30ec7d31267479
Russell Creswell <rncreswe@gmail.com>
2020-12-21 19:22:52 -0500

    Fixed new map issues

--- Fireteam AI/Assets/Scripts/Editor/GameControllerEditor.cs
+++ Fireteam AI/Assets/Scripts/Editor/GameControllerEditor.cs
@@ -5,20 +5,20 @@ using UnityEditor;
 [CustomEditor(typeof(GameControllerScript))]
 public class GameControllerEditor : Editor
 {
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
+    // public override void OnInspectorGUI()
+    // {
+    //     DrawDefaultInspector();
 
-        // if(GUILayout.Button("Assign All"))
-        // {
-        //     GameControllerScript myScript = (GameControllerScript) target;
-        //     Terrain[] lights = (Terrain[]) GameObject.FindObjectsOfType (typeof(Terrain));
-        //     myScript.terrainMetaData = new Terrain[lights.Length];
-        //     for (int i = 0; i < lights.Length; i++) {
-        //         lights[i].index = i;
-        //         myScript.terrainMetaData[i] = lights[i];
-        //     }
-        // }
+    //     if(GUILayout.Button("Assign All"))
+    //     {
+    //         GameControllerScript myScript = (GameControllerScript) target;
+    //         Terrain[] lights = (Terrain[]) GameObject.FindObjectsOfType (typeof(Terrain));
+    //         myScript.terrainMetaData = new Terrain[lights.Length];
+    //         for (int i = 0; i < lights.Length; i++) {
+    //             lights[i].index = i;
+    //             myScript.terrainMetaData[i] = lights[i];
+    //         }
+    //     }
 
-    }
+    // }
 }
