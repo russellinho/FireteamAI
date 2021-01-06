cf56936a2ac034455ba535ec6e63a2a5c2fd7dc7
Russell Creswell <rncreswe@gmail.com>
2021-01-06 02:19:01 -0500

    Replaced bomb model

--- Fireteam AI/Assets/Scripts/BombScript.cs
+++ Fireteam AI/Assets/Scripts/BombScript.cs
@@ -14,8 +14,6 @@ public class BombScript : MonoBehaviour {
 		
 	public void Defuse() {
 		defused = true;
-		GetComponent<MeshRenderer> ().material.color = Color.white;
-		GetComponentInChildren<SpriteRenderer> ().enabled = false;
 	}
 
 }
